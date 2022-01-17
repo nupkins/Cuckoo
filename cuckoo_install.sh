497c9b6602d28ce883851b9aa031edba8d540101
nupkins <97860195+nupkins@users.noreply.github.com>
2022-01-17 13:37:43 +1100

    Update cuckoo_install.sh

--- cuckoo_install.sh
+++ cuckoo_install.sh
@@ -7,15 +7,15 @@ sudo usermod -aG sudo cuckoo
 sudo apt update
 sudo apt upgrade
 
-sudo apt install python python3-pip python-dev libffi-dev libssl-dev
-sudo apt install python-setuptools
+sudo apt install python python3-pip python-dev libffi-dev libssl-dev -y
+sudo apt install python-setuptools -y
 pip3 install virtualenv
-sudo apt install libjpeg-dev zlib1g-dev swig
-sudo apt install mongodb
-sudo apt install postgresql libpq-dev
-sudo apt install virtualbox
-sudo apt install tcpdump apparmor-utils
-sudo apt install m2crypto
+sudo apt install libjpeg-dev zlib1g-dev swig -y
+sudo apt install mongodb -y
+sudo apt install postgresql libpq-dev -y
+sudo apt install virtualbox -y
+sudo apt install tcpdump apparmor-utils -y
+sudo apt install m2crypto -y
 
 
 sudo groupadd pcap
@@ -23,3 +23,10 @@ sudo usermod -a -G pcap cuckoo
 sudo chgrp pcap /usr/sbin/tcpdump
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
 sudo aa-disable /usr/sbin/tcpdump
+
+virtualenv -p /usr/bin/python2.7 cuckoo-env
+source cuckoo-env/bin/activate
+
+pip install setutools
+pip install -U cuckoo
+cuckoo init
